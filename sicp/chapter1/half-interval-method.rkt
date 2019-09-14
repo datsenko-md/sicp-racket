d1c3a6f2719acd9e398e53ce900b41c85fdf1689
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-14 13:44:10 +0300

    add half-interval-method

---
+++ sicp/chapter1/half-interval-method.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+(#%require "average.rkt")
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond ((positive? test-value) (search f neg-point midpoint))
+                ((negative? test-value) (search f midpoint pos-point))
+                (else midpoint))))))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else (error "У аргументов одинаковые знаки" a b)))))
+
+(#%provide half-interval-method)
+
+;(half-interval-method sin 2.0 4.0)
+;(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3)) 1.0 2.0)
+        

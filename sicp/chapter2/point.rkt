46c8f02323736727def8047f16969e1797fed853
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-16 17:24:24 +0300

    add point

---
+++ sicp/chapter2/point.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (print-point p)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(#%provide make-point
+           x-point
+           y-point
+           print-point)

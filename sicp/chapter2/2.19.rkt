2494a44680fdc255804e7c6cbd58fd9453b933cc
Maxim Datsenko <datsenko.md@gmail.com>
2019-09-30 20:56:17 +0300

    add 2.19

---
+++ sicp/chapter2/2.19.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+(define (no-more? items)
+  (null? items))
+
+(define (except-first-denomination items)
+  (cdr items))
+
+(define (first-denomination items)
+  (car items))
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(check-equal? (cc 100 us-coins) 292)

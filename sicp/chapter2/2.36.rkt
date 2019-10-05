84fbd356f9df426d193a358880f6b839ae3d65dc
Maxim Datsenko <datsenko.md@gmail.com>
2019-10-05 18:49:15 +0300

    add 2.36

---
+++ sicp/chapter2/2.36.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+(#%require rackunit
+           "accumulate.rkt")
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(check-equal? (accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12))) (list 22 26 30))
